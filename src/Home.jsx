b6a31de1eeb9ef188ac453bb327cc21e07c0f286
e-lizar <lizkamau55@gmail.com>
2026-06-29 17:34:43 +0300

    Fix home image paths

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
+import helloKitty from "./assets/two-piece-hello-kitty-set.jpeg";
+import ribbonSkirt from "./assets/ribbon-skirt.jpeg";
+import backlessDress from "./assets/backless-dress.jpeg";
+import shoulderBag from "./assets/shoulder-bag.jpeg";
 
 function Home() {
   const [greeting, setGreeting] = useState("");
@@ -21,26 +25,26 @@ function Home() {
     id: 1,
     name: "Crochet Hello Kitty bikini ",
     price: "$45",
-    image: "src/assets/two-piece-hello-kitty-set.jpeg"
+    image: "helloKitty"
   },
   {
     id: 2,
     name: "Crochet Skirt Ribbon",
     price: "$28",
-    image: "src/assets/ribbon-skirt.jpeg"
+    image: "ribbonSkirt"
   },
   {
     id: 3,
     name: "Croche Mini dress",
     price: "$20",
-    image: "src/assets/backless-dress.jpeg"
+    image: "backlessDress"
   },
     
   {
     id: 4,
     name: "Croche Bag",
     price: "$75",
-    image: "src/assets/shoulder-bag.jpeg"
+    image: "shoulderBag"
   }
 ];
   return (
